690895d53672b1207c500d99193f15c1b9ac4991
Lucas Brasil <lucas_2603@hotmail.com>
2026-01-09 22:12:24 -0300

    feat: primeiro commit do site Charllene Marcondes

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list')
+
+menuToggle.addEventListener("click", (e) =>{
+    e.stopPropagation()
+    navList.classList.toggle("active")
+})
+
+// Fecha o menu ao clicar fora
+document.addEventListener("click", (e) =>{
+    const ClickFora = navList.contains(e.target)
+    const ClickBotao = menuToggle.contains(e.target)
+
+    if (!ClickBotao && !ClickFora) {
+        navList.classList.remove("active")
+    }
+})
+
+//  Fecha o menu ao apertar o Esc
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape"){
+        navList.classList.remove("active")
+    }
+})
+
+// Fecha ao clicar nos links do Menu
+
+navList.querySelectorAll("a").forEach(link =>{
+    link.addEventListener("click", () =>{
+        navList.classList.remove("active")
+    })
+})
